3bca5b52b48b40d523e49da720c9ec3774568e60
Wilma <wilmareistad@icloud.com>
2026-05-22 09:32:35 +0200

    fixed style

--- src/errors/TransactionFailed.tsx
+++ src/errors/TransactionFailed.tsx
@@ -10,7 +10,7 @@ export default function TransactionFailed({
     <div className={styles.errorWrapper}>
       <h1>Payment failed</h1>
       <h2>Something went wrong when processing your payment.</h2>
-      <h3>Make sure to access this game from Tivoli to play again.</h3>
+      <h3 className={styles.subtitle}>Make sure to access this game from Tivoli to play again.</h3>
       <div className={styles.buttonContainer}>
         <button onClick={onRetry}>Try again</button>
         <button onClick={() => (window.location.href = tivoliUrl)}>
