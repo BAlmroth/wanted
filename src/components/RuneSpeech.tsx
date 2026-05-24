6bd53b6c633c74a8503ed37cc7cdc81b59ca30ee
Wilma <wilmareistad@icloud.com>
2026-05-24 13:01:01 +0200

    fixed rune and the speechbuuble into one img in figma for better look

--- src/components/RuneSpeech.tsx
+++ src/components/RuneSpeech.tsx
@@ -1,21 +1,13 @@
 import type { ReactNode } from "react";
 import styles from "./RuneSpeech.module.css";
-import HappyRuneImg from "../assets/HappyRune.png";
-import SpeechBubbleImg from "../assets/speechbubble.png";
+import speechbubbleAndRune from "../assets/speechbubbleAndRune.png";
 
 export function RuneSpeech(): ReactNode {
   return (
     <div className={styles.welcomeCard}>
       <img
-        src={SpeechBubbleImg}
-        alt="Speech bubble"
-        className={styles.speechBubble}
-      />
-      <p className={styles.welcomeText}>Welcome!</p>
-      <img
-        src={HappyRuneImg}
-        alt="Happy rune"
-        className={styles.happyRune}
+        src={speechbubbleAndRune}
+        alt="Rune say Welcome"
       />
     </div>
   );
